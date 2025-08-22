b37090948e6b4f6df85f48fdfadf01b2a289cbb0
Beancow <avery.shedden@gmail.com>
2025-08-22 16:14:39 +0100

    test: configure ts-jest transform for TSX in providers

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,17 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.test.tsx'],
     moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@/providers/(.*)$': '<rootDir>/src/providers/$1',
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@/providers/(.*)$': '<rootDir>/src/providers/$1',
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: {
+                jsx: 'react-jsx'
+            }
+        }
+    },
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
     },
 };
