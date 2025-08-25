11a63f67b19a2c68c3b3373b024d9c23c846878c
Beancow <avery.shedden@gmail.com>
2025-08-25 01:52:48 +0100

    refactor(storybook): update preview setup to use decorators export; enhance Story context handling

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Preview } from '@storybook/react';
 import { MockAppProvider, GlobalSeed } from '../src/storybook/mocks';
 import { Theme } from '@radix-ui/themes';
 import * as RadixTooltip from '@radix-ui/react-tooltip';
 import '@radix-ui/themes/styles.css';
 
-const preview: Preview = {
-    decorators: [
-        (Story) => (
-            <Theme>
-                <RadixTooltip.Provider>
-                    <MockAppProvider>
-                        <GlobalSeed>
-                            <div style={{ padding: 24 }}>
-                                <Story />
-                            </div>
-                        </GlobalSeed>
-                    </MockAppProvider>
-                </RadixTooltip.Provider>
-            </Theme>
-        ),
-    ],
-};
+export const decorators = [
+    (Story, context) => (
+        <Theme>
+            <RadixTooltip.Provider>
+                <MockAppProvider>
+                    <GlobalSeed>
+                        <div style={{ padding: 24 }}>
+                            <Story {...context} />
+                        </div>
+                    </GlobalSeed>
+                </MockAppProvider>
+            </RadixTooltip.Provider>
+        </Theme>
+    ),
+];
 
-export default preview;
+export const parameters = {
+    // add storybook parameters here if needed
+};
